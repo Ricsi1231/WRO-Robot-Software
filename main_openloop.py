36dd1575b7bad799d57a5eaea0c18865c0a65b31
Ricsi1231 <richardnagy551@gmail.com>
2026-05-15 18:59:11 +0200

    fix(project): update streering

--- main_openloop.py
+++ main_openloop.py
@@ -9,8 +9,8 @@ from wro.config import MotionConfig, MotorConfig, SteeringConfig
 from wro.motion_controller import MotionController
 from wro.motor_driver import MotorDriver
 
-RIGHT_DURATION_S: float = 1.0
-LEFT_DURATION_S: float = 1.0
+RIGHT_DURATION_S: float = 0.5
+LEFT_DURATION_S: float = 0.5
 STEERING_ACTIVATION_S: float = 3.0
 STEERING_OFF_S: float = 1.0
 CYCLES: int = 5
